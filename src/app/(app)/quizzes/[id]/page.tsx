4e8639a8b7e4fee484bb3e777d32da170ab19c8e
Tech World <techworldinfo98@gmail.com>
2025-07-14 13:34:35 +0000

    after add this "output: 'export'"

--- src/app/(app)/quizzes/[id]/page.tsx
+++ src/app/(app)/quizzes/[id]/page.tsx
@@ -5,3 +5,7 @@ import { TakeQuizPlayer } from './TakeQuizPlayer';
 export default function TakeQuizPage() {
     return <TakeQuizPlayer />;
 }
+
+export async function generateStaticParams() {
+    return [];
+}
--- src/app/(app)/quizzes/[id]/results/page.tsx
+++ src/app/(app)/quizzes/[id]/results/page.tsx
@@ -5,3 +5,7 @@ import { QuizResults } from './QuizResults';
 export default function QuizResultsPage() {
     return <QuizResults />;
 }
+
+export async function generateStaticParams() {
+    return [];
+}
